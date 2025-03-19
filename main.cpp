b3b1a491e58007a5ae5736fec22d8ee8878049f0
Vale088 <valerio.sinni@marconibari.edu.it>
2025-03-19 17:38:02 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n,i,j,temp;
+    cout<<"Quante cifre vuoi inserire?"<<endl;
+    cin>>n;
+    while(n<=0){
+    cout<<i+1<<"inserire un valore maggiore di 0"<<<endl;
+    cin>>n;
+    cout<<i+1<<"Numero:"<<r[i]<<endl;
+    } 
+    int r[n];
+    for(i=0;i<=n-1;i++) { 
+        r[i]=rand() & 11;
+
+    } 
+    for (i=0;i<=n-1;i++) {
+
+    for (j=0; j<=n-2;j++){
+        if(r[i]<r[j]){
+            temp=r[i];
+            r[i]=r[j];
+            r[j]=temp;
+        }
+    }
+} 
+} 
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
